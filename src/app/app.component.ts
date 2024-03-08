ac4d31fcd1725688eecbc757da07f4f0fc70b7cc
jdantashn <161704908+jdantashn@users.noreply.github.com>
2024-03-08 14:49:01 +0000

    added navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,46 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterLinkActive, RouterOutlet } from '@angular/router';
 import { NameInputComponent } from './name-input/name-input.component';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { TransactionsComponent } from './transactions/transactions.component';
+import { TransactionDetailComponent } from './transaction-detail/transaction-detail.component';
+import { RouterLink } from '@angular/router';
+import {Subscription } from "rxjs";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NameInputComponent,CalculatorComponent, TransactionsComponent],
+  imports: [  RouterOutlet,
+              NameInputComponent,
+              CalculatorComponent, 
+              TransactionsComponent, 
+              TransactionDetailComponent,
+              RouterLink,
+              RouterLinkActive
+            ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Exos Angular';
 
+
+  time = new Date();
+  intervalId = 0;
+  subscription: Subscription = new Subscription();
+
+  ngOnInit() {
+    // Using Basic Interval
+    this.intervalId = +setInterval(() => {
+      this.time = new Date();
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { TransactionDetailComponent } from './transaction-detail/transaction-detail.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { NameInputComponent } from './name-input/name-input.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'app-name-input', component: NameInputComponent },
+    { path: 'app-calculator', component: CalculatorComponent },
+    { path: 'app-transactions', component: TransactionsComponent },
+    { path: 'transaction-detail', component: TransactionDetailComponent }
+
+]
+    ;
+
+
